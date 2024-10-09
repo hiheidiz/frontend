b503e73defa47ab79232940962ecb61b4e04714a
hiheidiz <162917691+hiheidiz@users.noreply.github.com>
2024-10-09 09:45:20 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello world!
-{/*         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,10 +16,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>*/}
+        </a>
       </header>
     </div>
-  ); 
+  );
 }
-
 export default App;
